1181243ff9c011128cdb50abb27470cf5d671df4
ChargerIIC <ChargerIIC@Yahoo.com>
2017-07-06 13:34:18 -0700

    Adding Roster Page

---
+++ src/pages/roster/roster.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RosterPage } from './roster';
+
+@NgModule({
+  declarations: [
+    RosterPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RosterPage),
+  ],
+  exports: [
+    RosterPage
+  ]
+})
+export class RosterPageModule {}
